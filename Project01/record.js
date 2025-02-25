235402c61b5e5e5c8453ca70cbda9faef0897c3e
unknown <castroconi101@gmail.com>
2025-02-25 10:41:36 +0800

    With JavaScript 0.1

--- Project01/record.js
+++ Project01/record.js
@@ -1,3 +1,23 @@
-document.getElementById("vbtn").onclick = function() {
+var r = true;
+let recordButton = document.getElementById("vbtn");
+let recordStr = document.getElementById("recordStore");
+
+recordButton.onclick = function() {
     console.log("shshs");
+    if (r){
+        RecordStore();
+    }else {
+        recordButton.classList.add("rcdButton");
+        recordButton.classList.remove("vbtnActive");
+        recordButton.classList.add("recStore");
+        recordStr.classList.remove("recStoreOpen");
+        r = true;
+    }
+    }
+function RecordStore(){
+    recordButton.classList.remove("rcdButton");
+    recordButton.classList.add("vbtnActive");
+    recordButton.classList.remove("recStore");
+    recordStr.classList.add("recStoreOpen");
+    r = false;
 }
