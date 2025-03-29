f8f1ecfcc473b313b78dfe3eb489580955a1ef0d
unknown <castroconi101@gmail.com>
2025-03-30 00:27:34 +0800

    With working playable music

--- Project01/record.js
+++ Project01/record.js
@@ -4,6 +4,9 @@ let recordStr = document.getElementById("recordStore");
 let thrcds = document.getElementById("records");
 let maxScroll;
 
+let startTime = document.getElementById("startTime");
+let endTime = document.getElementById("endTime");
+
 recordButton.onclick = function() {
     console.log("shshs");
     if (r){
@@ -53,13 +56,45 @@ let pause = document.getElementById("pause");
 play.onclick = function(){
     pause.style.display = "block";
     play.style.display  = "none";
+    document.getElementById("audio").play();
 }
 pause.onclick = function(){
     pause.style.display = "none";
     play.style.display  = "block";
+    document.getElementById("audio").pause();
 }
 let slider = document.getElementById("slider");
 
 slider.addEventListener("input", function(){
- slider.style.background = `linear-gradient(to right, #000000 ${slider.value}%, #ffffff 0%)`;  
+ slider.style.background = `linear-gradient(to right, #000000 ${slider.value}%, #ffffff 0%)`; 
+ console.log(slider.value);
+ audio.currentTime = (slider.value / 100) * audio.duration;
+ console.log(audio.currentTime);
+ audio.pause();
 })
+
+audio.addEventListener('loadedmetadata', () => {
+    slider.max = audio.duration;
+    console.log(audio);
+    updateTimeDisplay();
+  });
+
+audio.addEventListener('timeupdate', () => {
+    slider.value = (audio.currentTime / audio.duration) * 100;
+    slider.style.background = `linear-gradient(to right, #000000 ${slider.value / 1}%, #ffffff 0%)`; 
+    updateTimeDisplay()
+  });
+
+
+  function formatTime(time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
+
+  function updateTimeDisplay() {
+    const current = formatTime(audio.currentTime);
+    const duration = audio.duration ? formatTime(audio.duration) : '00:00';
+    startTime.textContent = `${current}`;
+    endTime.textContent = `${duration}`;
+  }
